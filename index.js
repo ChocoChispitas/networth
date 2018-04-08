e07eedef53b88f3f091145e59a3c91851921b15b
Chispitas <Carolina@Carolinas-MacBook-Air.local>
2018-04-08 11:52:29 +0100

    html to public and fixed server

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require( 'express' );
 const app = express();
-var root = 'html/'
+var root = 'public/'
  
 app.use( express.static( root ) );
 app.listen( 3000, () => console.log( 'Hello world app listening on port 3000!' ) )
 
 console.log('Server started');
-
-$("#grid-basic").bootgrid();
