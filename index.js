de9954436326fd86a265c75a3804eea414274e6e
Chispitas <Carolina@Carolinas-MacBook-Air.local>
2018-07-16 11:19:13 +0100

    Drag and  drop

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require( 'express' );
 const app = express();
 var root = 'public/'
+app.get("/somethingBetter", function(req, res){
+    console.log (req);
+    res.send( ' here it is ' );
+});
  
 app.use( express.static( root ) );
 app.listen( 3000, () => console.log( 'Hello world app listening on port 3000!' ) )
